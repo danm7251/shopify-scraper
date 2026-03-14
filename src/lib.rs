bd41faa82234d6cb91a4222d8c99fa1e2341be77
Daniel Maynard <danielmaynard7@outlook.com>
2026-03-14 13:13:15 +0000

    Basic API

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use worker::*;
+use serde_json;
+
+#[event(fetch)]
+async fn main(
+    _req: Request,
+    _env: Env,
+    _ctx: Context,
+) -> Result<Response> {
+    let router = Router::new();
+
+    router
+    .get_async("/extract", |req, ctx| async move {
+        Response::ok("Extracting!")
+    })
+    .or_else_any_method_async("/:catchall*", |req, ctx| async move {
+        let body = serde_json::json!({
+            "error": "Not Found",
+            "status": 404
+        });
+        Response::from_json(&body)
+    })
+    .run(_req, _env).await
+}
